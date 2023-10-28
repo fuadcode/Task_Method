68a4ba884351c8f5247bf4e1d8ddc701c191e37a
Fuad <fuadgi@code.edu.az>
2023-10-29 01:25:26 +0400

    TaskMethodHomework

---
+++ TaskMethod/TaskMethod/Program.cs
@@ -0,0 +1,66 @@
+﻿#region task1
+//int number = 21;
+//bool NumberDivision(int stage)
+//{
+//    return stage % 21 == 0 ? true : false;
+//}
+//Console.WriteLine(NumberDivision(number));
+#endregion
+#region task2
+
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+//string Sum(int num, int num1)
+//{
+
+//    if (num % 2 == 0 && num1 % 2 == 0)
+//    {
+//        int result = num + num1;
+//        return ($"bu ededin cemi {result} ");
+//    }
+//    return "cut deyil";
+//}
+//Console.WriteLine(Sum(n, m));
+#endregion
+#region task3
+//int num1 = 3;
+//int num2 = 32;
+//int increase = default(int);
+
+//string tekeded(int num, int num1, int increase)
+//{
+//    for (int i = num; i < num1; i++)
+//    {
+//        if (num < num1)
+//        {
+//            if (i % 2 == 1)
+//            {
+//                increase++;
+
+//            }
+//        }
+//    }
+//    return ($"arasindaki reqem sayi {increase}");
+//}
+//Console.WriteLine(tekeded(num1, num2, increase));
+#endregion
+#region task4
+//string sum1(int n, int m)
+//{
+//    int sum1 = default(int);
+//    if (m > n)
+//    {
+//        for (int i = n; i <= m; i++)
+//        {
+//            if (i % 2 == 1)
+//            {
+//                sum1 += i;
+//            }
+//        }
+//        return $"{n} ve {m} ededleri arasindaki tek ededlerin cemi {sum1} olur";
+//    }
+//    return $"1-ci eded 2-ci ededden kicikdir";
+//}
+//string sumResult = sum1(12, 24);
+//Console.WriteLine(sumResult);
+#endregion
